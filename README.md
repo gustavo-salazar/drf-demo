6a0a487ecd5ec2f0bcc3900d8f033d53b9865f16
Gustavo A. Salazar <gustavoadolfo.salazar@gmail.com>
2021-05-10 12:33:24 +0100

    Lyrics API endpoint

---
+++ README.md
@@ -0,0 +1,5 @@
+Django REST Framework demo
+===
+
+This repo is the companion demo for the talk available in https://gustavo-salazar.github.io/ProteinFamiliesTalks/drf2021.html
+
--- api/serializers.py
+++ api/serializers.py
@@ -1,7 +1,13 @@
 from rest_framework import serializers
-from www.models import Artist
+from www.models import Artist, Song
 
 class ArtistSerializer(serializers.ModelSerializer):
   class Meta:
     model = Artist
     fields = ['id', 'name']
+
+class SongSerializer(serializers.ModelSerializer):
+  artist = serializers.StringRelatedField(many=False)
+  class Meta:
+    model = Song
+    fields = ['id', 'name', 'lyrics', 'artist' ]
